5c33aaae6e9f35027f14cbae1b49d7680e6c27b5
zhanshenzhi2008 <lj2010vip@163.com>
2024-10-28 11:40:29 +0800

    chore add spring ai

---
+++ src/main/java/com/orjrs/spring/ai/lab/SpringAiLabApplication.java
@@ -0,0 +1,13 @@
+package com.orjrs.spring.ai.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/orjrs/spring/ai/lab/SpringAiLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orjrs.spring.ai.lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
